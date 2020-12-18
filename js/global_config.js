cbe30719cb9c76a9448a9295fa42e1a852fef04e
LuYihui <lu_yihui@126.com>
2020-12-18 17:27:54 +0800

    图例颜色

--- js/global_config.js
+++ js/global_config.js
@@ -1,8 +1,12 @@
 // 图例字体大小
-var fontSize_all_legend = 18
+var fontSize_all_title = 18
+// 图例字体大小
+var fontSize_all_legend = 16
 // 坐标轴字体大小
 var fontSize_axis_label = 14
 // 坐标轴颜色，白色
 var color_axis = '#fff'
+// 图例颜色，白色
+var color_legend = '#fff'
 // echart toolbox字体大小
 var echart_toolbox_size = 16
