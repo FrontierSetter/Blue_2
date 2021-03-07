673be961bba69ba7506bf96ab55883abcfd427a3
LuYihui <lu_yihui@126.com>
2021-03-07 19:15:06 +0800

    字体加大&饼图年份

--- js/global_config.js
+++ js/global_config.js
@@ -1,9 +1,9 @@
+// 标题字体大小
+var fontSize_all_title = 22
 // 图例字体大小
-var fontSize_all_title = 20
-// 图例字体大小
-var fontSize_all_legend = 18
+var fontSize_all_legend = 20
 // 坐标轴字体大小
-var fontSize_axis_label = 16
+var fontSize_axis_label = 22
 // 坐标轴颜色，白色
 var color_axis = '#fff'
 // 图例颜色，白色
