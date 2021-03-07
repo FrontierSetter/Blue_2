3c1d827da15e2f97dd843077830e0e209d0135ae
Yang Guofeng <2201415225@qq.com>
2021-03-07 16:26:53 +0800

    3.7

--- js/global_config.js
+++ js/global_config.js
@@ -14,3 +14,12 @@ var echart_toolbox_size = 18
 var echart_lineStyle_width = 4
 // 折线图线宽-更宽
 var echart_lineStyle_width_large = 8
+
+// 气泡图xy轴字体大小
+var fontSize_bubble_xy_axis_label = 20
+// 气泡图时间轴字体大小
+var fontSize_bubble_time_axis_label = 20
+// 气泡图圆圈大小(比例)
+var symbolSize_bubble_circle = 1
+// 气泡图圆圈字体大小
+var fontSize_bubble_label = 20
